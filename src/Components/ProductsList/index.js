e790c14226ccbb26738f47447ab21428ba247609
Suseendhiran D <suseendhiran.d@f22labs.com>
2021-12-07 13:04:43 +0530

    add no items text

--- src/Components/ProductsList/index.js
+++ src/Components/ProductsList/index.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Product from "../Product";
 
 function Index({ products }) {
   return (
     <div className="prodcutsWrapper">
-      {products.map((product) => (
-        <Product product={product} />
-      ))}
+      {products.length ? (
+        products.map((product) => <Product product={product} />)
+      ) : (
+        <h2 style={{ textAlign: "center", width: "100%" }}>
+          No items Found !!
+        </h2>
+      )}
     </div>
   );
 }
